4a83c9b6b117e74ea8709fd0cbc17611b37314a3
LuckyMan612 <75744132+LuckyMan612@users.noreply.github.com>
2023-01-29 14:01:44 +0100

    Update script.js

--- Todo/script.js
+++ Todo/script.js
@@ -1,6 +1,8 @@
 const taskInput = document.getElementById("task-input");
 const addTaskButton = document.getElementById("add-task-button");
 const taskList = document.getElementById("task-list");
+const toggleCompletedButton = document.getElementById("toggle-completed-button");
+const completedList = document.getElementById("completed-list");
 
 addTaskButton.addEventListener("click", function() {
   const task = taskInput.value;
@@ -10,5 +12,14 @@ addTaskButton.addEventListener("click", function() {
   const taskItem = document.createElement("li");
   taskItem.textContent = task;
   taskList.appendChild(taskItem);
+  taskItem.addEventListener("click", function() {
+    taskItem.style.textDecoration = "line-through";
+    completedList.appendChild(taskItem);
+    completedList.style.display = "block";
+  });
   taskInput.value = "";
+});
+
+toggleCompletedButton.addEventListener("click", function() {
+  completedList.style.display = completedList.style.display === "none" ? "block" : "none";
 });
